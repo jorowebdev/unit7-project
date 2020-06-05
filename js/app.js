282ce485e3057d9f14af4547cb6696944c0399a5
jorowebdev <26332224+jorowebdev@users.noreply.github.com>
2020-06-04 22:58:45 -0700

    Add nav menu svg icons

--- js/app.js
+++ js/app.js
@@ -2,13 +2,16 @@ const navMenu = document.querySelector('.nav');
 const menuButton = document.querySelector('.header--menu-button');
 const navCloseButton = document.querySelector('.nav--close-button');
 const dashboardSection = document.querySelector('.dashboard');
+const footer = document.querySelector('.footer');
 
 menuButton.addEventListener('click', () => {
   navMenu.style.width = '62px';
   dashboardSection.style.marginLeft = '62px';
+  footer.style.marginLeft = '62px';
 });
 
 navCloseButton.addEventListener('click', () => {
   navMenu.style.width = '0';
   dashboardSection.style.marginLeft = '0';
+  footer.style.marginLeft = '0';
 });
