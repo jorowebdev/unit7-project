a7884967c7cd4936fc48acf4f037fd6aca7f23b7
jorowebdev <26332224+jorowebdev@users.noreply.github.com>
2020-07-10 20:09:05 -0700

    Add line chart and doughnut chart

--- js/app.js
+++ js/app.js
@@ -7,7 +7,11 @@ const notificationCardCloseBtn = document.querySelector('.header--notification-i
 const navCloseButton = document.querySelector('.nav--close-button');
 const dashboardSection = document.querySelector('.dashboard');
 const alertsWidget = document.querySelector('.alerts');
+const trafficChartNav = document.querySelector('.traffic--nav');
+const trafficChartLinks = document.querySelectorAll('.traffic--nav-link');
+const trafficCanvas = document.querySelector('#traffic-chart');
 const dailyCanvas = document.querySelector('#daily-chart');
+const mobileCanvas = document.querySelector('#mobile-chart');
 const autcompleteInput = document.querySelector('#userField');
 const messageAlert = document.querySelector('.message-alert');
 const messageAlertText = document.querySelector('#messageAlert');
@@ -100,6 +104,120 @@ alertsWidget.addEventListener('click', (e) => {
   }
 })
 
+const trafficDataHourly = {
+  labels: ['7 PM', '8 PM', '9 PM', '10 PM', '11 PM', '12 AM'],
+  datasets: [{
+    data: [800, 500, 1356, 1900, 1195, 900],
+    backgroundColor: 'rgba(116, 118, 191, .3)',
+    borderColor: '#7476bf',
+    borderWidth: 1,
+    lineTension: 0,
+    pointRadius: 6,
+    pointBackgroundColor: 'white',
+    pointBorderWidth: 2
+  }]
+}
+
+const trafficDataDaily = {
+  labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+  datasets: [{
+    data: [1500, 1550, 1675, 1800, 2000, 2400, 2500],
+    backgroundColor: 'rgba(116, 118, 191, .3)',
+    borderColor: '#7476bf',
+    borderWidth: 1,
+    lineTension: 0,
+    pointRadius: 6,
+    pointBackgroundColor: 'white',
+    pointBorderWidth: 2
+  }]
+}
+
+const trafficDataMonthly = {
+  labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  datasets: [{
+    data: [1100, 500, 750, 800, 600, 1500, 1200, 1356, 1000, 678, 500, 1300],
+    backgroundColor: 'rgba(116, 118, 191, .3)',
+    borderColor: '#7476bf',
+    borderWidth: 1,
+    lineTension: 0,
+    pointRadius: 6,
+    pointBackgroundColor: 'white',
+    pointBorderWidth: 2
+  }]
+}
+
+trafficChartNav.addEventListener('click', (e) => {
+  e.target.style.backgroundColor = '#80C98F';
+  e.target.style.color = 'white';
+  for (i = 0; i < trafficChartLinks.length; i++) {
+    if (trafficChartLinks[i].id != e.target.id) {
+      trafficChartLinks[i].style.backgroundColor = '';
+      trafficChartLinks[i].style.color = 'black';
+      trafficChartLinks[i].style.color = '';
+    }
+  }
+  if(e.target.id == 'hourly') {
+    trafficChart.data = trafficDataHourly;
+  } else if (e.target.id == 'daily') {
+    trafficChart.data = trafficDataDaily;
+  } else if (e.target.id == 'monthly') {
+    trafficChart.data = trafficDataMonthly;
+  } else {
+    trafficChart.data = trafficData;
+  }
+  trafficChart.update();
+})
+
+let trafficData = {
+  labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+  datasets: [{
+    data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+    backgroundColor: 'rgba(116, 118, 191, .3)',
+    borderColor: '#7476bf',
+    borderWidth: 1,
+    lineTension: 0,
+    pointRadius: 6,
+    pointBackgroundColor: 'white',
+    pointBorderWidth: 2
+  }]
+}
+
+const trafficOptions = {
+  aspectRatio: 2.5,
+  animation: {
+    duration: 0
+  },
+  scales: {
+    yAxes: [{
+      gridLines: {
+        drawTicks: false
+      },
+      ticks: {
+        beginAtZero: true,
+        padding: 10
+      }
+    }],
+    xAxes: [{
+      gridLines: {
+        drawTicks: false
+      },
+      ticks: {
+        padding: 10
+      }
+    }]
+  },
+  legend: {
+    display: false
+  }
+}
+
+let trafficChart = new Chart(trafficCanvas, {
+  type: 'line',
+  data: trafficData,
+  options: trafficOptions
+})
+
+
 const dailyData = {
   labels: ["S", "M", "T", "W", "Th", "F", "Sa"],
   datasets: [{
@@ -107,7 +225,6 @@ const dailyData = {
       data: [75, 105, 180, 125, 245, 210, 110],
       backgroundColor: '#7476bf',
       borderWidth: 1,
-      hoverBackgroundColor: '#4d4c72'
   }]
 };
 
@@ -115,7 +232,7 @@ const dailyOptions = {
   scales: {
     yAxes: [{
       gridLines: {
-        drawTicks: false,
+        drawTicks: false
       },
       ticks: {
         beginAtZero: true,
@@ -143,7 +260,35 @@ let dailyChart = new Chart(dailyCanvas, {
   options: dailyOptions
 });
 
+const mobileData = {
+  labels: ["Desktop", "Tablet", "Phones"],
+  datasets: [{
+      label: '# of Users',
+      data: [2000, 550, 500],
+      borderWidth: 0,
+      backgroundColor: [
+        '#7476bf',
+        '#80C98F',
+        '#74b1bf'
+      ]
+  }]
+};
 
+const mobileOptions = {
+  legend: {
+    position: 'right',
+    labels: {
+      boxwidth: 20,
+      fontstyle: 'bold'
+    }
+  }
+}
+
+const mobileChart = new Chart(mobileCanvas, {
+  type: 'doughnut',
+  data: mobileData,
+  options: mobileOptions
+});
 
 
 
